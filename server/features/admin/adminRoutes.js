8dc2a9bcef0a0d6611032d489b7108dc1ca16b60
Brian Hudson <brianjasonhudson@gmail.com>
2017-08-18 09:32:41 -0500

    Removed jwt for demo purposes

--- server/features/admin/adminRoutes.js
+++ server/features/admin/adminRoutes.js
@@ -6,8 +6,10 @@ const driverCtrl = require('../drivers/driverCtrl.js');
 module.exports = app => {
 
   app.route( '/api/admin/' )
-    .post( jwt({secret: new Buffer(config.auth0.secret, 'base64'), audience: config.auth0.audience})
-      , (req, res, next) => {
+    .post( 
+      //  jwt({secret: new Buffer(config.auth0.secret, 'base64'), audience: config.auth0.audience})
+      // , 
+      (req, res, next) => {
           console.log("Admin: ", req.body);
           next()
         }
